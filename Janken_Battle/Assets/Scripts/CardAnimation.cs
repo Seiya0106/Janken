3d57d381940f2e82577bef381acbcf56ce1174e3
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-03 17:50:06 +0900

    Add; CardAnimation

---
+++ Janken_Battle/Assets/Scripts/CardAnimation.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private int originalSiblingIndex;
+    void Start()
+    {
+        // 元のインデックスを保存
+        originalSiblingIndex = transform.GetSiblingIndex();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // カードを全面に移動
+        transform.SetAsLastSibling();
+
+        transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // カードを元の位置に戻す
+        transform.SetSiblingIndex(originalSiblingIndex);
+
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+}
