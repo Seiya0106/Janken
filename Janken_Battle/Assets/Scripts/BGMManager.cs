f8cd3d4833e206d336d13b0406218eb1a54aee7d
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-03 12:18:35 +0900

    Add; AudioManager

---
+++ Janken_Battle/Assets/Scripts/BGMManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public static BGMManager Instance { get; private set; }
+    private void PlayBGM(string bgmName)
+    {
+        
+    }
+}
---
+++ Janken_Battle/Assets/Scripts/SEManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SEManager : MonoBehaviour
+{
+    public static SEManager Instance { get; private set; }
+    private void PlaySE(string seName)
+    {
+        
+    }
+}
