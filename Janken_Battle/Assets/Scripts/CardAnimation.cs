b5e17834c1b7d67ff359a62fb789e4fc99f9319a
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-04 09:25:11 +0900

    Add; ドラッグ&ドロップ

--- Janken_Battle/Assets/Scripts/CardAnimation.cs
+++ Janken_Battle/Assets/Scripts/CardAnimation.cs
@@ -3,8 +3,13 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class CardAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+                            , IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private int originalSiblingIndex;
+    private Transform cardTransform;
+    private Vector2 prevPos;
+    [SerializeField] private GameObject prevParent;
+    public RectTransform dropZone;
     void Start()
     {
         // 元のインデックスを保存
@@ -23,5 +28,30 @@ public class CardAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         transform.SetSiblingIndex(originalSiblingIndex);
 
         transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // 元の位置と親を保存
+        prevPos = transform.position;
+        prevParent = transform.parent.gameObject;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Vector2 dropZonePos = dropZone.InverseTransformPoint(eventData.position);
+        if (dropZone.rect.Contains(dropZonePos))
+        {
+            transform.SetParent(dropZone);
+            GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        }
+        // ドロップ先がない場合、元の位置に戻す
+        else
+        {
+            transform.SetParent(prevParent.transform);
+            transform.position = prevPos;
+        }
     }
 }
---
+++ Janken_Battle/Assets/Scripts/DropZone.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+    }
+}
