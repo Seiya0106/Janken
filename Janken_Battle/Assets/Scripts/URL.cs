c637542fc316237a49977c71418327f5bc89bd57
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-09 11:27:48 +0900

    Add; urlの拡大

--- Janken_Battle/Assets/Scripts/URL.cs
+++ Janken_Battle/Assets/Scripts/URL.cs
@@ -1,9 +1,24 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class URL : MonoBehaviour, IPointerDownHandler
+public class URL : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
+    private TextMeshProUGUI urlText;
     public string url;
+    void Awake()
+    {
+        urlText = GetComponent<TextMeshProUGUI>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        urlText.rectTransform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        urlText.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
         OpenURL();
